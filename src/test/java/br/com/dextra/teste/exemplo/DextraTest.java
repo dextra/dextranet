62dc57b9f4ee39a49f9c6d1cf38f0d29d90e9407
Leandro Guimarães <leguimas@gmail.com>
2012-12-12 17:27:16 -0200

    Saucelabs test

--- src/test/java/br/com/dextra/teste/exemplo/DextraTest.java
+++ src/test/java/br/com/dextra/teste/exemplo/DextraTest.java
@@ -10,6 +10,10 @@ public class DextraTest extends TesteFuncionalBase {
 
 	@Test
 	public void testaBuscaDextraSistemas() {
+		System.out.println("SELENIUM_PLATFORM: " + System.getProperty("SELENIUM_PLATFORM"));
+		System.out.println("SELENIUM_VERSION : " + System.getProperty("SELENIUM_VERSION"));
+		System.out.println("SELENIUM_BROWSER : " + System.getProperty("SELENIUM_BROWSER"));
+
 		siteDextra.navegarNaPagina("http://www.dextra.com.br");
 		siteDextra.preencheInputText("input#gsc-i-id1", "java");
 		siteDextra.clica("input.gsc-search-button");
