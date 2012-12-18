e2a00ffa677977d0d6eaa82d32818b38595f5dbe
estagio_2013 <estagio_2013@trn3.(none)>
2012-12-18 11:50:51 -0200

    Ajuste links lateral esquerda

--- src/main/java/br/com/dextra/dextranet/rest/config/Application.java
+++ src/main/java/br/com/dextra/dextranet/rest/config/Application.java
@@ -3,7 +3,6 @@ package br.com.dextra.dextranet.rest.config;
 import java.util.HashSet;
 import java.util.Set;
 
-import br.com.dextra.exemplo.ExemploRS;
 
 public class Application extends javax.ws.rs.core.Application {
 
@@ -14,7 +13,7 @@ public class Application extends javax.ws.rs.core.Application {
 	@Override
 	public Set<Class<?>> getClasses() {
 		Set<Class<?>> classes = new HashSet<Class<?>>();
-		classes.add(ExemploRS.class);
+		//classes.add(ExemploRS.class);
 		return classes;
 	}
 
