5bac51cb862a3131a530530d1d3e340ce5d0d09c
Leticia Domingues <leticia.domingues@hotmail.com>
2012-12-20 11:16:19 -0200

    Organizacao do HTML e do js em pastas/Teste selenium criar post

--- src/test/java/br/com/dextra/testeSeleniumHomePage/DextraNetTest.java
+++ src/test/java/br/com/dextra/testeSeleniumHomePage/DextraNetTest.java
@@ -6,20 +6,27 @@ import org.junit.Test;
 
 import br.com.dextra.teste.TesteFuncionalBase;
 
-public class DextraNetTest extends TesteFuncionalBase
-{
+public class DextraNetTest extends TesteFuncionalBase {
 	PaginaDextraNet siteDextraNet = new PaginaDextraNet(driver);
 
 	@Test
-	public void testaBuscaDextraNet()
+	public void testeHomePageDextraNet() {
+		siteDextraNet.navegarNaPagina("http://dextra-estagio-2013.appspot.com/");
+		siteDextraNet.verificaSeTemLogoHomePage();
+	}
+
+
+
+	@Test
+	public void testeCriarEBuscarPost()
 	{
 		siteDextraNet.navegarNaPagina("http://dextra-estagio-2013.appspot.com/");
-		verificaSeTemLogo();
-
+		siteDextraNet.clica("span.icon-sidebar-left-novopost");
+		siteDextraNet.preencheInputText("input#form-input-title", "Titulo teste");
+		siteDextraNet.preencheInputText("input#form-input-content", "Corpo teeste");
+		//siteDextraNet.clica("input#form-post-submit");
+		//siteDextraNet.verificaSeApareceuPostIncluido();
 	}
 
-	private void verificaSeTemLogo()
-	{
-		Assert.assertTrue(siteDextraNet.obtemElemento("span.icon-header-logo") != null);
-	}
+
 }
