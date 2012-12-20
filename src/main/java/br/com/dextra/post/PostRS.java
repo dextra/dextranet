530d489821e42fc07cd77628d7a533c5ac54f9f6
pinaaapp <lucasmpina@hotmail.com>
2012-12-20 08:24:52 -0200

    Classe PostRS

---
+++ src/main/java/br/com/dextra/post/PostRS.java
@@ -0,0 +1,30 @@
+package br.com.dextra.post;
+
+import javax.ws.rs.FormParam;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+@Path("/post")
+public class PostRS {
+
+	@Path("/novo")
+	@POST
+	@Produces("application/json;charset=UTF-8")
+	public void novoPost(@FormParam("title") String titulo,
+			@FormParam("content") String conteudo) {
+		String usuario = "usuarioTeste";
+
+		Post novoPost = new Post();
+		novoPost.criaNovoPost(titulo, conteudo, usuario);
+		System.out.println("Inserido? "+novoPost.pegaDadosCorretos(titulo, conteudo, usuario));
+	}
+
+	@Path("/lista")
+	@GET
+	@Produces("application/json;charset=UTF-8")
+	public void listaPosts() {
+
+	}
+}
