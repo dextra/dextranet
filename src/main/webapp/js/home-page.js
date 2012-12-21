5038eb2b4301bb389521467e8e7d928430bce232
Leticia Domingues <leticia.domingues@hotmail.com>
2012-12-21 10:39:56 -0200

    Mensagem de sucesso

--- src/main/webapp/js/home-page.js
+++ src/main/webapp/js/home-page.js
@@ -3,16 +3,54 @@ function carregaDadosHomePage()
 	$.holy("../template/carrega-menu-principal.xml", {});
 	$.holy("../template/carrega-menu-lateral.xml", {});
 	$.holy("../template/carrega-dados-usuario.xml", {});
-	$.holy("../template/carrega-campo-pesquisa.xml", {});
 
 	$.ajax( {
 		type : 'GET',
 		url : "/s/posts",
 		data : "max-results=20&q=",
 		success : function(jsonArrayPost) {
-			$.holy("../template/carrega-ultimos-posts.xml",  {"jsonArrayPost" : jsonArrayPost});
+			$.holy("../template/carrega-miolo-home-page.xml",
+					{
+						"jsonArrayPost" : jsonArrayPost,
+						"sucesso" : false
+					});
 		}
 	});
 
 	$(document).delay(1000);
+}
+
+function carregaDadosHomePage2(temNovoPost)
+{
+	$.holy("../template/carrega-menu-principal.xml", {});
+	$.holy("../template/carrega-menu-lateral.xml", {});
+	$.holy("../template/carrega-dados-usuario.xml", {});
+
+	$.ajax( {
+		type : 'GET',
+		url : "/s/posts",
+		data : "max-results=20&q=",
+		success : function(jsonArrayPost) {
+			$.holy("../template/carrega-miolo-home-page.xml",
+					{
+						"jsonArrayPost" : jsonArrayPost,
+						"sucesso" : temNovoPost
+					});
+		}
+	});
+
+	$(document).delay(1000);
+}
+
+function fazPesquisa()
+{
+	var textSearch = $('#form-search-input').val();
+	$.ajax({
+		type : 'GET',
+		url : "/s/posts",
+		data : "max-results=5&q=" + textSearch,
+		success : function(jsonArrayPost) {
+			$.holy("../template/HomePage.xml", {"jsonArrayPost" : jsonArrayPost});
+		}
+	});
 }
