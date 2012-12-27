dca6434ecc99348ec21cd1d5d63f553d79ffbd29
schionato@gmail.com <estagio_2013@trn3.(none)>
2012-12-27 15:40:31 -0200

    ajuste build local

--- src/main/webapp/js/post/post.js
+++ src/main/webapp/js/post/post.js
@@ -4,22 +4,16 @@ function abrePaginaNovoPost() {
 }
 
 function fazPesquisa() {
-	var textSearch = $('#form_search_input').val();
-	var dados = "max-results=20";
-	if (textSearch != "")
-		dados += "&q=\"" + textSearch + "\"";
-	$.ajax( {
-		type : 'GET',
-		url : "/s/post",
-		data : dados,
-		success : function(jsonArrayPost) {
-			$.holy("../template/carrega_miolo_home_page.xml", {
-				"jsonArrayPost" : jsonArrayPost,
-				"sucesso" : false
-			});
-		}
-	});
-	return false;
+	var query = $('#form_search_input').val();
+	var ehUmNovoPost = false;
+	var pagina = 0
+
+	if(query != ""){
+		query = "\"" + query + "\"";
+		busquePosts(query, ehUmNovoPost, pagina);
+	}
+
+	return false; //O retorno falso faz com que a página de pesquisa não sofra reload para index
 }
 
 function criaNovoPost() {
@@ -61,75 +55,55 @@ function converteData(minhaData) {
 	var hora = minhaData.slice(10, 16);
 
 	switch (mes) {
-	case "Jan":
-		mes = "01";
+	case "Jan":	mes = "01";
 		break;
-	case "Fer":
-		mes = "02";
+	case "Fer":	mes = "02";
 		break;
-	case "Mar":
-		mes = "03";
+	case "Mar":	mes = "03";
 		break;
-	case "Apr":
-		mes = "04";
+	case "Apr":	mes = "04";
 		break;
-	case "May":
-		mes = "05";
+	case "May":	mes = "05";
 		break;
-	case "Jun":
-		mes = "06";
+	case "Jun":	mes = "06";
 		break;
-	case "Jul":
-		mes = "07";
+	case "Jul":	mes = "07";
 		break;
-	case "Aug":
-		mes = "08";
+	case "Aug":	mes = "08";
 		break;
-	case "Sep":
-		mes = "09";
+	case "Sep":	mes = "09";
 		break;
-	case "Oct":
-		mes = "10";
+	case "Oct": mes = "10";
 		break;
-	case "Nov":
-		mes = "11";
+	case "Nov":	mes = "11";
 		break;
-	case "Dec":
-		mes = "12";
+	case "Dec":	mes = "12";
 		break;
 	}
 
 	switch (diaS) {
-	case "Sun":
-		diaS = "dom";
+	case "Sun":	diaS = "dom";
 		break;
-	case "Mon":
-		diaS = "seg";
+	case "Mon":	diaS = "seg";
 		break;
-	case "Tue":
-		diaS = "ter";
+	case "Tue":	diaS = "ter";
 		break;
-	case "Wed":
-		diaS = "qua";
+	case "Wed":	diaS = "qua";
 		break;
-	case "Thu":
-		diaS = "qui";
+	case "Thu":	diaS = "qui";
 		break;
-	case "Fri":
-		diaS = "sex";
+	case "Fri":	diaS = "sex";
 		break;
-	case "Sat":
-		diaS = "sab";
+	case "Sat":	diaS = "sab";
 		break;
 	}
 
 	return diaS + ", " + dia + "/" + mes + "/" + ano + " - " + hora;
 }
 
-function paginacaoDosPost() {
+function paginacaoDosPost(query) {
 
 	var pagina = 0;
-	var query = "";
 	var ehUmNovoPost = false;
 	var posicaoMinimaParaNovaPagina = posicaoDoScrollBuscarMaisPosts();
 	var margemParaNovaBusca = (document.documentElement.scrollHeight);
@@ -150,7 +124,7 @@ function paginacaoDosPost() {
 }
 
 function posicaoDoScrollBuscarMaisPosts() {
-	var maximoValorDoScroll =  document.documentElement.scrollHeight;
+	var maximoValorDoScroll = document.documentElement.scrollHeight;
 	var porcentagemDaPaginaDisparaNovaBusca = 0.80;
 
 	return (maximoValorDoScroll * (porcentagemDaPaginaDisparaNovaBusca));
