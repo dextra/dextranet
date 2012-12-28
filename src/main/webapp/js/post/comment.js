6e1ca1e7dbef7cdd2ede060b62ef4d63c9fd3817
Webster Lima <webster.lima@dextra-sw.com>
2012-12-28 16:56:53 -0200

    Primeira correção 'comentario'

--- src/main/webapp/js/post/comment.js
+++ src/main/webapp/js/post/comment.js
@@ -3,10 +3,27 @@ function abreTelaComentario(idDaDivDoPost) {
 	var deuCerto = false;
 	LIs.each(function() {
 		if ($(this).attr("class") == idDaDivDoPost) {
-			$(this).append("<br><div><form id='form_comment'><textarea id='textarea_comment' placeholder='Digite seu comentario' style='resize:none;'  /></form></div>");
+			$(this).append("<br><div align='center'><form id='form_comment'><textarea id='textarea_comment' placeholder='Digite seu comentario' style='resize:none;'  />" +
+					"<br><input type='submit' value='Comentar' /></form></div>");
 			deuCerto = true;
 		}
 	});
 
 	return deuCerto;
+}
+
+//Erros com o comentario:
+//	quando ele abrir outro textarea (nao na mesmo post)
+//	quando der um submit, ele deve ocultar
+
+function chamaAbreTela() {
+	var jaTemTextArea = false;
+	$(".link").click(function(){
+		if(!jaTemTextArea){
+			if(abreTelaComentario($(this).attr("id"))){
+				CKEDITOR.replace('textarea_comment');
+				jaTemTextArea = true;
+			}
+		}
+	});
 }
