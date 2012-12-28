6e9f4997716020f047c7da670a68f502c805756e
ederig <ignatowicz@gmail.com>
2012-12-28 08:45:03 -0200

    script improvement

--- scripts/criaPosts.sh
+++ scripts/criaPosts.sh
@@ -1,11 +1,12 @@
 #/bin/bash
-for n in {1..400}; do
+for (( n=0;n<$1;n++));do
   a=`expr $n % 2` 
     texto="Impar"
     if [ $a -eq 0 ] ; then   
       texto="Par"
     fi
-    wget --post-data "title=exemplo$n&content=conteudoExemplo$texto" http://localhost:8080/s/post
+    sleep 1
+	wget --post-data "title=exemplo$n&content=conteudoExemplo$texto" http://localhost:8080/s/post
 done; 
 rm post*
 
