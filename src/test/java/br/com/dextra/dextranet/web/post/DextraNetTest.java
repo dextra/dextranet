b961bc230494b3c952934b026d1c388a523aa3f4
ederig <ignatowicz@gmail.com>
2013-01-07 13:14:47 -0200

    Removendo o Lorem ipsum. Testes no cloudbees.

--- src/test/java/br/com/dextra/dextranet/web/post/DextraNetTest.java
+++ src/test/java/br/com/dextra/dextranet/web/post/DextraNetTest.java
@@ -3,21 +3,13 @@ package br.com.dextra.dextranet.web.post;
 import org.junit.Test;
 
 import br.com.dextra.teste.TesteFuncionalBase;
-import br.com.dextra.teste.utils.LoremIpsum4J;
 
 public class DextraNetTest extends TesteFuncionalBase {
-
-	private LoremIpsum4J loremIpsum = new LoremIpsum4J();
 
 	PaginaDextraNet siteDextraNet = new PaginaDextraNet(driver);
 
 	@Test
 	public void testeHomePageDextraNet() {
-		try {
-			Thread.sleep(60000);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
 		this.acessaPaginaPrincipal();
 		siteDextraNet.verificaSeTemLogoHomePage();
 	}
@@ -29,8 +21,8 @@ public class DextraNetTest extends TesteFuncionalBase {
 		siteDextraNet.click("span.icon_sidebar_left_novopost");
 		siteDextraNet.waitForElement("input#form_input_title");
 
-		siteDextraNet.writeInputText("input#form_input_title", loremIpsum.getWordsAsText(5));
-		siteDextraNet.writeCKEditor(loremIpsum.getParagraphsAsText(1));
+		siteDextraNet.writeInputText("input#form_input_title", "Titulo de Teste");
+		siteDextraNet.writeCKEditor("Comentario de Teste");
 
 		siteDextraNet.click("input#form_post_submit");
 		siteDextraNet.esperaCarregarPaginaInicial();
