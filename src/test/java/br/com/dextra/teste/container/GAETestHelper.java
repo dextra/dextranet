b0a358ac46171ce79e4704ee145938320bf8535b
paula.guatura@dextra-sw.com <paula.guatura@dextra-sw.com>
2013-01-08 18:02:42 -0200

    correção das alterações feitas no GAETestHelper para testes de autenticação

--- src/test/java/br/com/dextra/teste/container/GAETestHelper.java
+++ src/test/java/br/com/dextra/teste/container/GAETestHelper.java
@@ -54,8 +54,8 @@ public class GAETestHelper {
 		helper.setEnvAttributes(envs);
 		helper.setEnvIsLoggedIn(false);
 		helper.setEnvIsAdmin(false);
-		//helper.setEnvEmail("login.google@example.com");
-		//helper.setEnvAuthDomain("example.com");
+		helper.setEnvEmail("login.google@example.com");
+		helper.setEnvAuthDomain("example.com");
 	}
 
 	public void prepareSearchServiceTestHelper() throws Exception {
