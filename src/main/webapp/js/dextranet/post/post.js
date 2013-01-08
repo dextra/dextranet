e38079d8986c1ceb79f40a99cfd37a6a00f612cf
Gabriel Ferreira <gabriel.ferreira@dextra-sw.com>
2013-01-08 13:05:33 -0200

    Resolvendo merge

--- src/main/webapp/js/dextranet/post/post.js
+++ src/main/webapp/js/dextranet/post/post.js
@@ -1,20 +1,3 @@
-<<<<<<< HEAD
-teste = {};
-
-teste.fazPesquisa = function() {
-    		   var cons = $('#form_search_input').val();
-    		   consulta.setText(cons);
-    		   var ehUmNovoPost = false;
-    		   var pagina = 0;
-
-    		   if(consulta.getText() != ""){
-    			   dextranet.busquePosts(consulta.getText(), ehUmNovoPost, pagina);
-    		   }
-    		   //$("#form_gif_loading").css("display", "none");
-    		   return false; //O retorno falso faz com que a página de pesquisa não sofra reload para index
-
-};
-=======
 
 dextranet.post = {
 
