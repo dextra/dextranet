77783b2d2dd2c3fae821cb3762ad647e0f04a162
Webster Lima <webster.lima@dextra-sw.com>
2013-01-08 10:15:26 -0200

    Função setLoading dentro do object dextranet

--- src/main/webapp/js/dextranet/home.js
+++ src/main/webapp/js/dextranet/home.js
@@ -1,11 +1,17 @@
 
 var dextranet = {
 
-	loading : function() {
+	configuraLoading : function() {
 		$.loading({
 			text : 'Carregando...',
 			overlay : '#23557E',
 			opacity: '60'
 		});
-	}
+	},
+
+	setLoading : function() {
+		jQuery.ajaxSetup({
+			loading : true
+		});
+	},
 };
