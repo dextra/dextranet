928b0d193055e93e84287c4c9df88328926359e9
pinaaapp <lucasmpina@hotmail.com>
2013-01-09 08:18:35 -0200

    Alerts

--- src/main/webapp/js/dextranet/post/post.js
+++ src/main/webapp/js/dextranet/post/post.js
@@ -11,7 +11,6 @@ dextranet.post = {
 		consulta.setText(cons);
 
 		if(consulta.getText() != ""){
-			$("#form_gif_loading").css("display", "inline");
 			dextranet.post.listarPosts(consulta.getText(), ehUmNovoPost, pagina);
 		}
 		return false;
@@ -21,8 +20,6 @@ dextranet.post = {
 		var url = "/s/post";
 		var quantidadePostsSolicitados = "20";
 		var template = "../template/post.xml";
-
-		$("#form_gif_loading").css("display", "inline");
 
 		$.ajax( {
 			type : "GET",
@@ -60,8 +57,6 @@ dextranet.post = {
 				|| (CKEDITOR.instances.form_input_content.getData() == "")) {
 			$("li.warning").css("display", "list-item");
 		} else {
-			$("#form_post_submit").css("display", "none");
-			$("#form_gif_loading").css("display", "inline");
 
 			var post = {
 				"title" : $("#form_input_title").val(),
