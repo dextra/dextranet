789cca58a7f1f52aebfbaa1ce4fda0696c2ca957
schionato@gmail.com <estagio_2013@trn3.(none)>
2013-01-11 13:45:38 -0200

    adição de reconhecimento se carregou o último json

--- src/main/webapp/js/dextranet/paginacao/paginacao.js
+++ src/main/webapp/js/dextranet/paginacao/paginacao.js
@@ -3,28 +3,77 @@ dextranet.paginacao = {}
 
 dextranet.paginacao.paginacaoDosPosts=function() {
 
-	$(window).scroll(function (){
-		var ehUmNovoPost = false;
-		var posicaoMinimaParaNovaPagina = posicaoDoScrollBuscarMaisPosts();
-		var posicaoDoScroll = $(document).scrollTop();
+	var ehUmNovoPost = false;
+	var scroll = new dextranet.paginacao.scroll();
 
-		console.info("scroll: " + posicaoDoScroll + "\n posicaoMinimaParaNovaPagina: " + posicaoMinimaParaNovaPagina);
-		if (posicaoDoScroll > posicaoMinimaParaNovaPagina) {
-				dextranet.post.listarPosts(consulta.getText(), ehUmNovoPost, numeroDaPagina.getPagina());
-				numeroDaPagina.next();
-				console.info(numeroDaPagina.getPagina());
+	$(window).scroll(function (){
+
+		if (scroll.solicitarNovaPagina()) {
+			dextranet.post.listarPosts(consulta.getText(), ehUmNovoPost, numeroDaPagina.getPagina());
+			numeroDaPagina.next();
+			console.info(numeroDaPagina.getPagina());
 		}
 	})
-
-	function posicaoDoScrollBuscarMaisPosts()
-	{
-		var maximoValorDoScroll = document.documentElement.scrollHeight;
-		var porcentagemDaPaginaDisparaNovaBusca = 0.65;
-
-		return (maximoValorDoScroll * (porcentagemDaPaginaDisparaNovaBusca));
-	}
 };
 
+dextranet.paginacao.scroll = function() {
+
+	var posicaoMaximaQueOScrollChegou = 0;
+	var chegouAteOFinal = true;
+
+	getPosicao = function(){
+		return posicaoDoScroll = $(document).scrollTop();
+	}
+
+	proximaPosicaoScrollParaRecarregar = function(){
+		var maximoValorDoScroll = document.documentElement.scrollHeight;
+		var porcentagemDaPaginaDisparaNovaBusca = 0.65;
+		return (maximoValorDoScroll * (porcentagemDaPaginaDisparaNovaBusca));
+	}
+
+	this.solicitarNovaPagina = function(){
+		if (getPosicao() > proximaPosicaoScrollParaRecarregar()){
+			scrollChegouAteOFinal();
+			return true;
+		}
+		else{
+			return false;
+		}
+	}
+
+	scrollChegouAteOFinal = function(){
+		var url = "/s/post";
+		var quantidadePostsSolicitados = "20";
+		var busca = {
+				"max-results" : quantidadePostsSolicitados,
+				"page" : numeroDaPagina.getPagina(),
+				"q" : consulta.getText()
+		};
+
+		$.ajax( {
+			type : "GET",
+			url : url,
+			data : busca,
+			success : function(posts) {
+				if(posts.length > 0){
+					setChegouAteOFinal(false);
+				}
+				else{
+					setChegouAteOFinal(true);
+				}
+			}
+		});
+	}
+
+	setChegouAteOFinal = function(status){
+		console.info(status);
+		chegouAteOFinal = status;
+	}
+
+	this.getChetouAteOFinal = function(){
+		return chegouAteOFinal;
+	}
+}
 
 
 
