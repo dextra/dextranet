2e2b32d46dc723e7bd72e1ec4e168680bfa7cf2e
schionato@gmail.com <estagio_2013@trn3.(none)>
2013-01-11 10:12:36 -0200

    Paginação funcionando no Chrome.

--- src/main/webapp/js/dextranet/paginacao/paginacao.js
+++ src/main/webapp/js/dextranet/paginacao/paginacao.js
@@ -6,12 +6,11 @@ dextranet.paginacao.paginacaoDosPosts=function() {
 	$(window).scroll(function (){
 		var ehUmNovoPost = false;
 		var posicaoMinimaParaNovaPagina = posicaoDoScrollBuscarMaisPosts();
-		var margemParaNovaBusca = (document.documentElement.scrollHeight*0.95);
 		var posicaoDoScroll = $(document).scrollTop();
 
+		console.info("scroll: " + posicaoDoScroll + "\n posicaoMinimaParaNovaPagina: " + posicaoMinimaParaNovaPagina);
 		if (posicaoDoScroll > posicaoMinimaParaNovaPagina) {
 				dextranet.post.listarPosts(consulta.getText(), ehUmNovoPost, numeroDaPagina.getPagina());
-				posicaoMinimaParaNovaPagina = (posicaoDoScroll + margemParaNovaBusca);
 				numeroDaPagina.next();
 				console.info(numeroDaPagina.getPagina());
 		}
@@ -20,7 +19,7 @@ dextranet.paginacao.paginacaoDosPosts=function() {
 	function posicaoDoScrollBuscarMaisPosts()
 	{
 		var maximoValorDoScroll = document.documentElement.scrollHeight;
-		var porcentagemDaPaginaDisparaNovaBusca = 0.80;
+		var porcentagemDaPaginaDisparaNovaBusca = 0.65;
 
 		return (maximoValorDoScroll * (porcentagemDaPaginaDisparaNovaBusca));
 	}
