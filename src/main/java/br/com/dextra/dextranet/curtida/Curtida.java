7b7006e52970f38cf853b410aecc5d8c8800eb1c
Kaique de Souza Monteiro <kaique.monteiro@dextra-sw.com>
2013-01-15 09:20:26 -0200

    Implementação da classe Curtida

--- src/main/java/br/com/dextra/dextranet/curtida/Curtida.java
+++ src/main/java/br/com/dextra/dextranet/curtida/Curtida.java
@@ -1,5 +1,38 @@
 package br.com.dextra.dextranet.curtida;
 
+import br.com.dextra.dextranet.utils.Data;
+
+
+
 public class Curtida {
+
+	private String data;
+	private String idPost;
+	private String usuarioLogado;
+
+	public Curtida(String usuario, String id) {
+
+		this.idPost = id;
+		this.usuarioLogado = usuario;
+	}
+
+
+	public String getData(){
+		Data data = new Data();
+		this.data = data.pegaData();
+		return this.data;
+
+	}
+
+	public String getIdPost() {
+		return idPost;
+	}
+
+
+	public String getUsuarioLogado() {
+	return usuarioLogado;
+
+	}
+
 
 }
