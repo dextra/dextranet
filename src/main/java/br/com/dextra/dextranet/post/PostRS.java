6a639ddfe685eff5e24776947617691c879a22ea
Marco Bordon <marco.bordon@dextra-sw.com>
2013-01-15 09:41:10 -0200

    like put

--- src/main/java/br/com/dextra/dextranet/post/PostRS.java
+++ src/main/java/br/com/dextra/dextranet/post/PostRS.java
@@ -11,6 +11,7 @@ import javax.ws.rs.DefaultValue;
 import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -64,7 +65,7 @@ public class PostRS {
 	}
 
 	@Path("/")
-	@POST
+	@PUT
 	@Produces("application/json;charset=UTF-8")
 	public Response comentar(@FormParam("usuario") String usuario, @FormParam("id") String id) throws EntityNotFoundException {
 		Post post = new PostRepository().obtemPorId(id);
