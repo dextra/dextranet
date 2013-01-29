f7d67ce77eaa32c9b1a02c0fbf00fc56270a034f
Webster Lima <webster.lima@dextra-sw.com>
2013-01-29 16:22:58 -0200

    Callback do GAE em REST

--- src/main/java/br/com/dextra/dextranet/banner/BannerRS.java
+++ src/main/java/br/com/dextra/dextranet/banner/BannerRS.java
@@ -1,9 +1,12 @@
 package br.com.dextra.dextranet.banner;
 
 import java.io.IOException;
+import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
@@ -21,12 +24,13 @@ public class BannerRS {
 
 	private BlobstoreService blobstoreService = BlobstoreServiceFactory
 			.getBlobstoreService();
+	private BannerRepository bannerRepository = new BannerRepository();
 
 	@Path("/uploadURL")
 	@GET
 	@Produces("application/json;charset=UTF-8")
-	public String upload() throws EntityNotFoundException {
-		String url = blobstoreService.createUploadUrl("/banner/upload");
+	public String uploadURL() throws EntityNotFoundException {
+		String url = blobstoreService.createUploadUrl("/s/banner/");
 		JsonObject json = new JsonObject();
 		json.addProperty("url", url);
 		
@@ -37,7 +41,6 @@ public class BannerRS {
 	@GET
 	@Produces("image/*")
 	public Response BannerAtualURL(@Context HttpServletResponse response) {
-		BannerRepository bannerRepository = new BannerRepository();
 		Banner bannerAtual = bannerRepository.getBannerAtual();
 
 		if (bannerAtual == null || bannerAtual.getBlobKey() == null)
@@ -55,4 +58,20 @@ public class BannerRS {
 
 		return Response.ok().build();
 	}
+	
+	@Path("/")
+	@POST
+	public void retornoDoGAE(@Context HttpServletRequest request, @Context HttpServletResponse response) throws IOException {
+		@SuppressWarnings("deprecation")
+		Map<String, BlobKey> blobs = blobstoreService.getUploadedBlobs(request);
+        BlobKey blobKey = blobs.get("banner");
+
+        if (blobKey != null) {
+            bannerRepository.criar(new Banner(request.getParameter("bannerTitle"), blobKey));
+        } else 
+        	response.setStatus(500);
+
+    	response.sendRedirect("/");
+	}
+	
 }
