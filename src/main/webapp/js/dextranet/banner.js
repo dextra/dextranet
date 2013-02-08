3de0241d7d843de1b60e19178b33904448a89fb9
alexis.barbieri <alexis.barbieri@dextra-sw.com>
2013-02-08 16:36:18 -0200

    Finalizado a ordenacao de banners

--- src/main/webapp/js/dextranet/banner.js
+++ src/main/webapp/js/dextranet/banner.js
@@ -75,6 +75,7 @@ dextranet.banner = {
 						$(bannerObjectArray).each(function() {
 							this.postObjectJson.dataInicio = converteData(this.postObjectJson.dataInicio).substring(5,15);
 							this.postObjectJson.dataFim = converteData(this.postObjectJson.dataFim).substring(5,15);
+							this.postObjectJson.dataDeAtualizacao = converteData(this.postObjectJson.dataDeAtualizacao).substring(5,15);
 						});
 						$.holy("../template/dinamico/abre_pagina_edita_banner.xml", {"jsonArrayBanner" : bannerObjectArray});						
 					}
@@ -136,7 +137,20 @@ dextranet.banner = {
 			});
 		},
 
-		ordenaBanners : function(orderBy) {
-			alert("Ordering: " + orderBy);
+		ordenaBanners : function(orderBy, banners) {
+			var parentUl = $(banners).parent();
+			var elems = $(parentUl).children('li').remove();
+
+			elems.sort(function(a,b) {
+				var aDatePieces = $(a).find("[id^='" + orderBy + "']").val().split('/')
+				var aCompleteDate = aDatePieces[2] + aDatePieces[1] + aDatePieces[0];
+
+				var bDatePieces = $(b).find("[id^='" + orderBy + "']").val().split('/')
+				var bCompleteDate = bDatePieces[2] + bDatePieces[1] + bDatePieces[0];
+
+				return parseInt(aCompleteDate) < parseInt(bCompleteDate);
+			});
+			$(parentUl).append(elems);
+
 		}
 }
