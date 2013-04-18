3628ce08973dcabfc2fae373ba411cb8c2ede9be
Leandro Guimarães <leguimas@gmail.com>
2013-04-18 16:01:38 -0300

    Encodinf

--- src/main/webapp/js/dextranet/settings.js
+++ src/main/webapp/js/dextranet/settings.js
@@ -34,12 +34,12 @@ dextranet.settings = {
 		configuraDatePicker : function() {
 			$.datepicker.setDefaults({
 			    dateFormat: 'dd/mm/yy',
-			    dayNames: ['Domingo','Segunda','Terça','Quarta','Quinta','Sexta','Sábado'],
+			    dayNames: ['Domingo','Segunda','TerÃ§a','Quarta','Quinta','Sexta','SÃ¡bado'],
 			    dayNamesMin: ['D','S','T','Q','Q','S','S','D'],
-			    dayNamesShort: ['Dom','Seg','Ter','Qua','Qui','Sex','Sáb','Dom'],
-			    monthNames: ['Janeiro','Fevereiro','Março','Abril','Maio','Junho','Julho','Agosto','Setembro','Outubro','Novembro','Dezembro'],
+			    dayNamesShort: ['Dom','Seg','Ter','Qua','Qui','Sex','SÃ¡b','Dom'],
+			    monthNames: ['Janeiro','Fevereiro','MarÃ§o','Abril','Maio','Junho','Julho','Agosto','Setembro','Outubro','Novembro','Dezembro'],
 			    monthNamesShort: ['Jan','Fev','Mar','Abr','Mai','Jun','Jul','Ago','Set','Out','Nov','Dez'],
-			    nextText: 'Próximo',
+			    nextText: 'Prï¿½ximo',
 			    prevText: 'Anterior'
 			});
 		},
