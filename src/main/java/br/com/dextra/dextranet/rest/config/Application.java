f804fc6afe37ff47dd9c5ea2ba18fee03255c03e
Matheus Lopes <matheus.lopes@dextra-sw.com>
2013-05-20 09:19:00 -0300

    Limite de posts por página

--- src/main/java/br/com/dextra/dextranet/rest/config/Application.java
+++ src/main/java/br/com/dextra/dextranet/rest/config/Application.java
@@ -19,7 +19,7 @@ public class Application extends javax.ws.rs.core.Application {
 
 	public static final Locale BRASIL = new Locale("pt", "BR");
 
-	public static final String REGISTROS_POR_PAGINA = "5";
+	public static final String REGISTROS_POR_PAGINA = "20";
 
 	/**
 	 * Adicionando todas as classes referentes a servicos REST que irao existir
