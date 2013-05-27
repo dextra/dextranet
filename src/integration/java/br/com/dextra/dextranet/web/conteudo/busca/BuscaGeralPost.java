b35946fa31fbce754e60eb25f7f99a2f6daecc02
rsilvamagalhaes <rodrigo.magalhaes@dls97.lab.dextra.com.br>
2013-05-27 16:06:34 -0300

    Teste busca geral

--- src/integration/java/br/com/dextra/dextranet/web/conteudo/busca/BuscaGeralPost.java
+++ src/integration/java/br/com/dextra/dextranet/web/conteudo/busca/BuscaGeralPost.java
@@ -1,5 +1,22 @@
 package br.com.dextra.dextranet.web.conteudo.busca;
 
-public class BuscaGeralPost {
+import org.openqa.selenium.WebDriver;
 
+import br.com.dextra.dextranet.PaginaBase;
+
+public class BuscaGeralPost extends PaginaBase {
+
+	public BuscaGeralPost(WebDriver driver) {
+		super(driver);
+	}
+
+	public void redigeConteudoDaBusca(String conteudo) {
+		String idInputBusca = "form_search_input";
+		this.redigeTextoInput(conteudo, idInputBusca);
+	}
+
+	public void clicaNoBotaoPesquisa() {
+		this.click("input#form_search_submit");
+		waitingForLoading();
+	}
 }
--- src/integration/java/br/com/dextra/dextranet/web/conteudo/busca/BuscaGeralPostTesteFuncional.java
+++ src/integration/java/br/com/dextra/dextranet/web/conteudo/busca/BuscaGeralPostTesteFuncional.java
@@ -1,5 +1,7 @@
 package br.com.dextra.dextranet.web.conteudo.busca;
 
+import junit.framework.Assert;
+
 import org.junit.After;
 import org.junit.Test;
 
@@ -8,53 +10,65 @@ import br.com.dextra.dextranet.web.conteudo.post.PaginaNovoPost;
 import br.com.dextra.dextranet.web.conteudo.post.comentario.PaginaNovoComentario;
 
 public class BuscaGeralPostTesteFuncional  extends TesteFuncionalBase {
-	private PaginaNovoPost paginaNovoPost = null;
-	private PaginaNovoComentario paginaNovoComentario;
+	private static final String CONTEUDO_POST = "Conteudo do post";
+	private static final String TITULO_POST = "Titulo do post";
+	private static final int QTD_POSTS = 1;//TODO: aumentar para 2
+	private PaginaNovoPost paginaNovoPost;
 	private BuscaGeralPost buscaGeralPost;
 
-	@After
-	public void criaPostsEComentarios() {
-		paginaPrincipal.dadoQueUsuarioAcessaPaginaPrincipal();
-		paginaNovoComentario = new PaginaNovoComentario(driver);
-		eleCriaPostsEComentario();
+	public BuscaGeralPostTesteFuncional() {
+		buscaGeralPost =  new BuscaGeralPost(driver);
+		paginaNovoPost = new PaginaNovoPost(driver);
 	}
 
-	@Test
-	public void testaBuscaGeral() {
+//	@Test
+	public void testaBuscaGeralPorPost() {
+		paginaPrincipal.dadoQueUsuarioAcessaPaginaPrincipal();
+		eleCriaPostsEComentario();
 		eleFazBuscaPosts();
-		eleChecaSePostsEncontrados();
+
+		//entaoChecaSePostsEncontrados();
+	}
+
+	//@Test
+	public void testaBuscaGeralPorComentario() {
 		eleFazBuscaPostPorComentarios();
-		eleChecaSePostsEncontrados();
+//		eleChecaSePostsEncontrados();
 	}
 
 	private void eleFazBuscaPostPorComentarios() {
-		// TODO Auto-generated method stub
 
 	}
 
-	private void eleChecaSePostsEncontrados() {
-
-	}
-
-	private void eleFazBuscaPosts() {
-		// TODO Auto-generated method stub
-
-	}
-
-	private void eleCriaPostsEComentario() {
-		for (int cont = 0; cont < 2; cont++) {
-			String tituloPost = "Titulo de Teste " + cont;
-			String conteudo = "Texto do teste " + cont;
-
-			paginaNovoPost = paginaPrincipal.clicaEmNovoPost();
-			paginaNovoPost.criarNovoPost(tituloPost, conteudo);
-
-			criaComentarioParaPost(tituloPost);
+	private void entaoChecaSePostsEncontrados() {
+		for (int cont = 0; cont <= QTD_POSTS; cont++) {
+			String conteudo = CONTEUDO_POST + " " + cont;
+			String titulo = TITULO_POST + " " + cont;
+			Assert.assertTrue(paginaNovoPost.existePostPor(titulo, conteudo));
 		}
 	}
 
-	private void criaComentarioParaPost(String tituloPost) {
-		paginaNovoComentario.clicaEmNovoComentario();
+	private void eleFazBuscaPosts() {
+		buscaGeralPost.redigeConteudoDaBusca("titulo:" + TITULO_POST + " 0");
+		buscaGeralPost.clicaNoBotaoPesquisa();
+	}
+
+	private void eleCriaPostsEComentario() {
+		for (int cont = 0; cont <= QTD_POSTS; cont++) {
+			String tituloPost = TITULO_POST + " " + cont;
+			String conteudo = CONTEUDO_POST + " " + cont;
+
+			paginaNovoPost = paginaPrincipal.clicaEmNovoPost();
+			paginaNovoPost.criarNovoPost(tituloPost, conteudo);
+			paginaNovoPost.existePostPor(tituloPost, conteudo);
+			criaComentarioParaPost(tituloPost, paginaNovoPost.getIdPost());
+		}
+	}
+
+	private void criaComentarioParaPost(String tituloPost, String idPost) {
+		PaginaNovoComentario paginaNovoComentario = new PaginaNovoComentario(driver);
+		paginaNovoComentario.setIdPost(idPost);
+
 		String conteudo = "Texto do comentário.";
 		paginaNovoComentario.criaNovoComentario(conteudo);
 	}
