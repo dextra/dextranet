fcbd764ce6724cfdaf4d8c56ff4e60be77cb3e36
Matheus Lopes <matheus.lopes@dextra-sw.com>
2013-05-28 17:49:00 -0300

    Verifica posts recentes

--- src/main/webapp/js/lib/setInterval-utils.js
+++ src/main/webapp/js/lib/setInterval-utils.js
@@ -1,25 +1,24 @@
 setIntervalUtils = {
 
+	formataData : function (data){
+		d = data.split(" ");
+		dia = d[0].split("/");
+		hora = d[1].split(":");
+		//Date(yyyy, mm, dd, hh, mm, ss)
+		return data = new Date(dia[2],dia[1] - 1,dia[0],hora[0],hora[1], 59);
+
+	},
+
 	verificaPosts : function (data){
 		setInterval(function() {
-			d = ultimoPost.split(" ");
-			dia = d[0].split("/");
-			hora = d[1].split(":");
-			//Date(yyyy, mm, dd, hh, mm, ms)
-			data = new Date(dia[2],dia[1] - 1,dia[0],hora[0],hora[1]);
-			dextranet.post.contar(data);
+			dextranet.post.contar(setIntervalUtils.formataData(ultimoPost));
 		}, dextranet.settings.intervaloBuscaNovosPosts);
 
 	},
 
 	verificaMicroBlog : function (data){
 		setInterval(function() {
-			d = ultimoMicroPost.split(" ");
-			dia = d[0].split("/");
-			hora = d[1].split(":");
-			//Date(yyyy, mm, dd, hh, mm, ss, ms)
-			data = new Date(dia[2],dia[1] - 1,dia[0],hora[0],hora[1]);
-			dextranet.microblog.contar(data);
+			dextranet.microblog.contar(setIntervalUtils.formataData(ultimoMicroPost));
 		}, dextranet.settings.intervaloBuscaNovosPosts);
 
 	}
