c8948d93595f2f9742b61afad9ac1d6d2ac1888a
rsilvamagalhaes <rodrigo.magalhaes@dls97.lab.dextra.com.br>
2013-06-20 13:10:57 -0300

    serviços grupo.

---
+++ src/test/java/br/com/dextra/dextranet/grupo/UsuarioMembro.java
@@ -0,0 +1,25 @@
+
+package br.com.dextra.dextranet.grupo;
+
+public class UsuarioMembro {
+	private String id;
+	private String nome;
+
+	public UsuarioMembro(String id, String nome) {
+		this.id = id;
+		this.nome = nome;
+	}
+
+	public String getId() {
+		return id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+}
