bbce38caa8ca94e1824a0ecd190e2e3c01c5de91
Bruno Lacerda Camarini <bruno.camarini@dextra-sw.com>
2013-06-27 08:40:28 -0300

    Fonte

--- src/main/java/br/com/dextra/dextranet/microblog/MicroPost.java
+++ src/main/java/br/com/dextra/dextranet/microblog/MicroPost.java
@@ -75,7 +75,7 @@ public class MicroPost extends Entidade {
     		return text;
     	}
 
-    	String str = "(?i)\\b((?:https?://|www\\d{0,3}[.]|[a-z0-9.\\-]+[.][a-z]{2,4}/)(?:[^\\s()<>]+|\\(([^\\s()<>]+|(\\([^\\s()<>]+\\)))*\\))+(?:\\(([^\\s()<>]+|(\\([^\\s()<>]+\\)))*\\)|[^\\s`!()\\[\\]{};:\'\".,<>?«»“”‘’]))";
+    	String str = "(?i)\\b((?:https?://|www\\d{0,3}[.]|[a-z0-9.\\-]+[.][a-z]{2,4}/)(?:[^\\s()<>]+|\\(([^\\s()<>]+|(\\([^\\s()<>]+\\)))*\\))+(?:\\(([^\\s()<>]+|(\\([^\\s()<>]+\\)))*\\)|[^\\s`!()\\[\\]{};:\'\".,<>?]))";
     	Pattern patt = Pattern.compile(str);
     	Matcher matcher = patt.matcher(text);
     	text = matcher.replaceAll("<a href=\"$1\" target='_blank'>$1</a>");
