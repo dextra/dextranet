9f3484b464d6b3e0536b8b4472ff151ecc9894c5
rsilvamagalhaes <rodrigo.magalhaes@dls97.lab.dextra.com.br>
2013-07-02 08:05:51 -0300

    Servicos externos

---
+++ src/main/java/br/com/dextra/dextranet/servicos/GoogleGroups.java
@@ -0,0 +1,61 @@
+package br.com.dextra.dextranet.servicos;
+
+import java.util.List;
+
+import org.jboss.resteasy.client.ClientRequest;
+import org.jboss.resteasy.client.ClientResponse;
+
+import br.com.dextra.dextranet.grupo.UsuarioJSON;
+
+public class GoogleGroups implements ServicosExternos {
+	private List<String> gruposEmail;
+
+	public GoogleGroups(List<String> gruposEmail) {
+		this.gruposEmail = gruposEmail;
+	}
+
+	@Override
+	public void adicionaMembros(String token, List<UsuarioJSON> usuarios) throws Exception {
+		ClientRequest client = null;
+		for (String grupoEmail : gruposEmail) {
+			client = new ClientRequest("https://www.googleapis.com/admin/directory/v1/groups/[groupKey]/members".replace("[groupKey]", grupoEmail));
+
+			for (UsuarioJSON usuarioJSON : usuarios) {
+				client.body("email", usuarioJSON.getEmail());
+			}
+
+			client.header("Authorization", "Bearer " + token);
+			client.execute();
+		}
+	}
+
+	@Override
+	public void removeMembros(String token, List<UsuarioJSON> usuarios) throws Exception {
+		ClientRequest client = null;
+		for (String grupoEmail : gruposEmail) {
+			for (UsuarioJSON usuario : usuarios) {
+				client = new ClientRequest("https://www.googleapis.com/admin/directory/v1/groups/" + grupoEmail + "/members/" + usuario);
+				client.header("Authorization", "Bearer " + token);
+				client.execute();
+			}
+		}
+	}
+
+	@Override
+	public void obtemMembro(String token, String grupoEmail, String emailUsuario) throws Exception {
+		ClientRequest client = null;
+		client = new ClientRequest("https://www.googleapis.com/admin/directory/v1/groups/" + grupoEmail + "/members/" + emailUsuario);
+		ClientResponse response = client.post();
+
+	}
+
+	@Override
+	public void listaMembros() {
+		// TODO Auto-generated method stub
+
+	}
+
+	private void configuraUsuarioRequest(List<UsuarioJSON> usuarios, ClientRequest client) {
+
+	}
+}
